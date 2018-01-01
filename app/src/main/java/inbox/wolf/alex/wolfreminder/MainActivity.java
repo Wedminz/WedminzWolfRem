5b55bca200896a9c2a6ec304db36fd609131b61e
Alexw <alex.wolf.inbox@gmail.com>
2018-01-02 00:24:14 +0200

    Create View Pager, tabs, tab adapter and fragment example

--- app/src/main/java/inbox/wolf/alex/wolfreminder/MainActivity.java
+++ app/src/main/java/inbox/wolf/alex/wolfreminder/MainActivity.java
@@ -1,16 +1,21 @@
 package inbox.wolf.alex.wolfreminder;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import inbox.wolf.alex.wolfreminder.adapter.TabsPagerFragmentAdapter;
+
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     private static final int LAYOUT = R.layout.activity_main;
     DrawerLayout drawerLayout;
+    ViewPager viewPager;
 
 
     @Override
@@ -21,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
         initToolbar();
         initNavigationView();
+        initTabs();
+
+
 
     }
 
@@ -39,5 +47,13 @@ public class MainActivity extends AppCompatActivity {
     private void initNavigationView() {
         drawerLayout = findViewById(R.id.drawer_layout);
 
+    }
+
+    private void initTabs() {
+        viewPager = findViewById(R.id.viewPager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/inbox/wolf/alex/wolfreminder/fragment/ExampleFragment.java
@@ -0,0 +1,28 @@
+package inbox.wolf.alex.wolfreminder.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import inbox.wolf.alex.wolfreminder.R;
+
+public class ExampleFragment extends Fragment {
+
+    private static final int LAYOUT = R.layout.fragment_example;
+    private View view;
+
+    public static ExampleFragment getInstance(){
+        Bundle args = new Bundle();
+        ExampleFragment fragment = new ExampleFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(LAYOUT, container, false);
+        return view;
+    }
+}
