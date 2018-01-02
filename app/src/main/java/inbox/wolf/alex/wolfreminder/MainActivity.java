4a297047376f787bc2b4ce67c4c397b75d0cc919
Alexw <alex.wolf.inbox@gmail.com>
2018-01-02 18:45:33 +0200

    Update and create Navigation Drawer. Add listener for menu and start Notification activity

--- app/src/main/java/inbox/wolf/alex/wolfreminder/MainActivity.java
+++ app/src/main/java/inbox/wolf/alex/wolfreminder/MainActivity.java
@@ -1,12 +1,13 @@
 package inbox.wolf.alex.wolfreminder;
 
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 
 import inbox.wolf.alex.wolfreminder.adapter.TabsPagerFragmentAdapter;
@@ -48,6 +49,24 @@ public class MainActivity extends AppCompatActivity {
     private void initNavigationView() {
         drawerLayout = findViewById(R.id.drawer_layout);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigation_open
+                , R.string.view_navigation_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()){
+                    case R.id.actionNavigationItem:
+                        showNotificationTab();
+                }
+                return true;
+            }
+        });
+
     }
 
     private void initTabs() {
@@ -56,5 +75,10 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
         TabLayout tabLayout = findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+    private void showNotificationTab(){
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 }
